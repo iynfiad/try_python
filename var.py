8c65d5b5b5f372f0264204044416af466e92a9d8
Iyn Fiad <iynfiad14@gmail.com>
2025-06-17 14:48:10 +0700

    varpy

---
+++ var.py
@@ -0,0 +1,25 @@
+#Variabel
+
+a = 10
+x = 5
+long = "long var"
+
+#first calling
+print("a = ", a)
+print("x = ", x)
+print("long = ", long)
+
+#name
+value_pi = 3.14 #using underscore
+mil10 = 10000000 #number cannot be first
+valueZ = 150
+
+#second calling
+print("mil10 = ", mil10)
+a = 17
+print("a = ", a)
+print("valueZ = ", valueZ)
+
+#assignment indirect
+b = value_pi
+print("b = ", b)
